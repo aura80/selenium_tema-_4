93e6f32271255a7e0d9e0a4de22bebc983d1ed44
aura80 <96388713+aura80@users.noreply.github.com>
2022-04-30 16:59:42 +0300

    Update test_xpaths.py

--- test_automation_school/test_xpaths.py
+++ test_automation_school/test_xpaths.py
@@ -2,7 +2,10 @@ import time
 import selenium.webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def test1():
@@ -11,7 +14,7 @@ def test1():
 
     driver.maximize_window()
 
-    driver.find_element(By.XPATH, '/html/body/div[1]/div/div[3]/div[2]/div[2]/form/div[2]/input').send_keys("tricou")
+    driver.find_element(By.XPATH, '/html/body/div[1]/div/div[3]/div[2]/div[2]/form/div[2]/input').send_keys("abcd@yahoo.com")
     driver.find_element(By.XPATH, '/html/body/div[1]/div/div[3]/div[1]/img')
     span = driver.find_element(By.XPATH, '//*[@id="content"]/div[2]/span').text
     print(span)
@@ -60,8 +63,32 @@ def test5():
 
     driver.maximize_window()
 
-    #   OR
+    #   OR   -   AND   -   ActionChains
     driver.find_element(By.XPATH, '//input[@name="search_query" or placeholder="Search"]').send_keys("blouse")
     driver.find_element(By.XPATH, '//button[@class="btn btn-default button-search" or name="submit_search"]').click()
+    driver.find_element(By.XPATH, '//select[@id="selectProductSort" and @class="selectProductSort form-control"]').click()
+
+    driver.get("http://automationpractice.com/index.php?controller=search&search_query=blouse&submit_search=&orderby=quantity&orderway=desc")
+
+    element = driver.find_element(By.XPATH, '//option[@value="quantity:desc"]')
+
+    actions = ActionChains(driver)
+
+    actions.move_to_element(element).click()
+
+    time.sleep(4)
+
+def test6():
+    driver = selenium.webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get("http://automationpractice.com/index.php")
+
+    driver.maximize_window()
+
+    #   contains    -    starts-with
+    driver.find_element(By.XPATH, '//input[contains(@id,"_query_")]').send_keys("skirt")
+    driver.find_element(By.XPATH, '//button[starts-with(@name,"submit_")]').click()
+
+    driver.get("http://automationpractice.com/index.php?controller=search&orderby=position&orderway=desc&search_query=skirt&submit_search=")
+    driver.find_element(By.XPATH, "//a[text()='Women']").click()
 
     time.sleep(4)
